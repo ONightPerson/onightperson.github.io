66ac5bcef4ab9d27fb5cb1e18a7df76ee39775a5
liubaozhu02 <liubaozhu02@meituan.com>
2023-03-06 10:13:34 +0800

    feat: bridge

--- demo/index.js
+++ demo/index.js
@@ -1,10 +1,24 @@
-window.onload = function() {
-  let result;
-  if (window.android) {
-    result = window.android.back();
-  } else {
-    result = 'Hello, Hybrid World'
-  }
-  document.getElementById('main-content').innerHTML = result
- 
+const callbacks = {};
+let uniqueId = 0;
+function callHandler(name, data, callback) {
+  const id = `cb_${uniqueId++}_${new Date().getTime()}`;
+  callbacks[id] = callback;
+  window.bridge.send(name, JSON.stringify(data), id);
 }
+window.bridge.onReceive = function(callbackId, result) {
+  console.log('callbackId', callbackId, 'result', result);
+}
+window.onload = function () {
+  callHandler("notify", { data: { name: "liubaozhu", addr: "suiping" }}, 
+  {
+    success: function() {
+    console.log("yes");
+  }});
+  let result;
+  if (window.bridge) {
+    result = window.bridge.back();
+  } else {
+    result = "Hello, Hybrid World";
+  }
+  document.getElementById("main-content").innerHTML = result;
+};
